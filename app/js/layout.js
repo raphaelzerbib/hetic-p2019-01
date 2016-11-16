b3493e196946125265391693fe02fb4cf9efdf6e
Auree Aubert <contact@auree.me>
2016-11-16 21:25:06 +0100

    [mobile] fix bug on href

--- app/js/layout.js
+++ app/js/layout.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var aElements = document.querySelectorAll("a");
+    for (var i = 0; i < aElements.length; i++) {
+        aElements[i].addEventListener("touchend", function() {
+            window.location.href = this.getAttribute("href");
+        });
+    }
+
+})
+
 function navManager() {
     var self = this;
 
